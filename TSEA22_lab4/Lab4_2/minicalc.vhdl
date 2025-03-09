912f950f245989208b1367c72fe63a0b4a10cce2
andan346 <andan346@student.liu.se>
2025-03-09 21:37:52 +0100

    minicalc klar

--- TSEA22_lab4/Lab4_2/minicalc.vhdl
+++ TSEA22_lab4/Lab4_2/minicalc.vhdl
@@ -17,8 +17,95 @@ entity minicalc is
 end entity;
 
 architecture func of minicalc is
-  -- signals etc.
+
+  signal strobe_sync0, strobe_sync, strobe_ep : std_logic;
+  signal operator : unsigned(3 downto 0) := (others => '0');
+  signal number, result : unsigned(4 downto 0) := (others => '0');
+
+  type digit_rom is array (0 to 15) of std_logic_vector(6 downto 0);
+  constant to_display : digit_rom := (
+  "1000000", -- 0
+  "1111001", -- 1
+  "0100100", -- 2
+  "0110000", -- 3
+  "0011001", -- 4
+  "0010010", -- 5
+  "0000010", -- 6
+  "1111000", -- 7
+  "0000000", -- 8
+  "0010000", -- 9
+  "0001000", -- A
+  "0000011", -- b
+  "1000110", -- C
+  "0100001", -- d
+  "0000110", -- E
+  "0001110" -- F
+);
 
 begin
+
+  -- Synka och enpulsa strobe
+  process(clk) begin
+
+    if rising_edge(clk) then
+      strobe_sync0 <= strobe;
+      strobe_sync <= strobe_sync0;
+    end if;
+
+  end process;
+  strobe_ep <= strobe_sync0 and (not strobe_sync);
+
+  -- Process för nummer- och operator-manipulation
+  process(clk, reset)
+  begin
+
+    -- Reset
+    if reset = '1' then
+      -- Gör nåt här
+      
+    -- Vid strobe-puls
+    elsif rising_edge(clk) then
+      if strobe_ep = '1' then
+
+        -- (0-9) Indata är en siffra
+        if unsigned(data) <= to_unsigned(9, 4) then
+
+          -- Applicera operator
+          if operator = 15 then
+            result <= (number + unsigned(data));
+          elsif operator = 14 then
+            result <= (number - unsigned(data));
+          end if;
+
+          -- Sätt nummer till indata
+          number <= resize(unsigned(data), 5);
+
+        -- (F-C) Indata är en operator
+        elsif unsigned(data) >= to_unsigned(12, 4) then
+
+          -- (=) Sätt resultatet till senaste nummer
+          if unsigned(data) = to_unsigned(13, 4) then
+            number <= result;
+          end if;
+
+          -- (clr) Vid operator "clr"
+          if unsigned(data) = to_unsigned(12, 4) then
+            number <= "00000";
+            result <= "00000";
+          end if;
+
+          -- Sätt operator till indata
+          operator <= unsigned(data);
+
+        end if;
+        
+      end if;
+    end if;
+  end process;
+
+  -- Display
+  seg <= to_display(to_integer(number));
+  dp <= '1';
+  an <= "1110";
 
 end architecture;
